19cb76673b39377816ad2770a14da1748482d480
Sardor <asrorjon5538@gmail.com>
2021-01-20 17:14:55 +0500

    initial commit

--- crud-project/main.js
+++ crud-project/main.js
@@ -19,7 +19,6 @@ var todoListGroup = document.querySelector("#todo-list-group");
 var todoModalBody = document.querySelector("#todo-modal-body");
 var todoSaveButton = document.querySelector("#todo-save-button");
 var outputNum = document.querySelector('#output-order')
-
 var todoList = [];
 var todoInitialId = 0;
 
@@ -32,11 +31,19 @@ todoSaveButton.addEventListener('click', function (e) {
   e.preventDefault();
 
   todoList.forEach(function (item) {
-    var modalListItem = document.createElement('h5');
+    var modalListItem = document.createElement('h6');
     modalListItem.textContent = `${item.todoId+1}. ${item.todoText}`;
 
     todoModalBody.appendChild(modalListItem);
-    // modalListItem.reset()
+
+  })
+});
+document.querySelector('.modal').addEventListener('click', function (e) {
+
+  todoList.forEach(function (item) {
+    var modalListItem = document.querySelector('h6');
+    modalListItem.remove();
+
   })
 });
 
@@ -51,31 +58,77 @@ function removeTodoItem(todoId) {
   console.log(todoList);
 }
 
+function arrEditor(id, text) {
+  todoList.forEach(todoList => {
+    if (todoList.todoId === id) {
+      todoList.todoText = text
+    }
+  })
+}
+
+
 function createTodoHtml(todoText, todoId) {
   var listItem = document.createElement('li');
   var deleteButton = document.createElement('a');
+  let editButton = document.createElement('button')
+  let inputOutput = document.createElement('input')
 
-  listItem.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center');
-  listItem.textContent = todoText;
+  inputOutput.setAttribute('class', 'todo-input')
+
+  listItem.setAttribute('class', 'list-group-item flex-row-reverse d-flex justify-content-between align-items-center');
+  // listItem.textContent = todoText;
+  inputOutput.value = todoText;
+  inputOutput.disabled = true;
   listItem.setAttribute('id', `todo-item-${todoId}`);
-  icon = document.createElement('i')
-  icon.setAttribute('class', 'fad fa-trash-alt')
+
+  iconTrash = document.createElement('i')
+  iconTrash.setAttribute('class', 'fal fa-trash-alt')
+
+  iconAdd = document.createElement('i')
+  iconAdd.setAttribute('class', 'fal fa-edit')
+
+  editButton.setAttribute('class', 'btn btn-outline-success');
+  editButton.appendChild(iconAdd)
+  editButton.style.marginRight = '10px'
 
   deleteButton.setAttribute('href', 'javascript:void(0)');
   deleteButton.setAttribute('class', 'btn btn-outline-danger');
-  deleteButton.appendChild(icon)
+  deleteButton.appendChild(iconTrash)
 
   deleteButton.addEventListener('click', function (e) {
     e.preventDefault();
     outputNum.innerHTML = todoList.length - 1
     removeTodoItem(todoId);
+    if (todoList.length > 0) {
+      todoSaveButton.disabled = false 
+    } else {
+      todoSaveButton.disabled = true
+    }
 
   })
-
-
-  listItem.appendChild(deleteButton);
-
+  editButton.addEventListener('click', () => {
+    editButton.setAttribute("id", `${todoInitialId - 1}`);
+    inputOutput.disabled = false;
+    inputOutput.focus();
+    console.log(todoList);
+  })
+  var myBlock = document.createElement('div')
+  listItem.appendChild(myBlock);
+  // 
+  // Added to btns to an additional block
+  myBlock.appendChild(editButton);
+  myBlock.appendChild(deleteButton);
+  listItem.appendChild(inputOutput)
   todoListGroup.appendChild(listItem);
+  inputOutput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      console.log('works');
+      inputOutput.disabled = true
+      arrEditor(todoId, inputOutput.value)
+      console.log(todoList)
+    }
+
+  })
 }
 
 function createTodo(todoText) {
@@ -83,6 +136,7 @@ function createTodo(todoText) {
   createTodoHtml(todoText, todoInitialId);
   todoInitialId++;
 }
+
 
 todoForm.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -92,14 +146,19 @@ todoForm.addEventListener('submit', function (e) {
   } else {
     createTodo(todoInput.value);
   }
-  todoSaveButton.removeAttribute("disabled");
+  if (todoList.length > 0) {
+    todoSaveButton.disabled = false
+  } else {
+    todoSaveButton.disabled = true
+  }
+  // todoSaveButton.removeAttribute("disabled");
   document.querySelector('#output-order').innerHTML = todoList.length
 
-  
   console.log(todoList);
   todoForm.reset();
 });
-
+// 
+// function for close the toast warning
 $('.btn-close-toast').click(() => {
   $('.toast').removeClass("show")
 
